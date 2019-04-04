656a3d96f3e840b5dc337d86ba27f8276c0b9cc6
kmalhotra13 <k9@sukarm.com>
2019-04-04 09:48:37 -0400

    Update round-up-saver.py

--- apps/round-up-saver.py
+++ apps/round-up-saver.py
@@ -1,7 +1,23 @@
-# Round-Up Saver Application
-## The purpose of this application is to help the user save money incrementally, by rounding up the transactions they perform on a daily basis to the nearest dollar, and depositing those extra few pennies into their savings account. 
+# # Round-Up Saver Application
+# ## The purpose of this application is to help the user save money incrementally, by rounding up the transactions they perform on a daily basis to the nearest dollar, and depositing those extra few pennies into their savings account. 
 
 import os
+from datetime import datetime
+import json
+import requests
+# from dotenv import load_dotenv
+import banking_client
 
-###
+# load_dotenv() # loads environment variables set in a ".env" file, including the value of the ALPHAVANTAGE_API_KEY variable
 
+token_key = os.environ.get("token")
+secret_key = os.environ.get("secret")
+current_date = datetime.now()
+
+header = {
+	'token': token_key,
+	'secret': secret_key, 
+	'date': current_date
+}
+
+print(header)
