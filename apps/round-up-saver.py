85253109382d222e5c92e5ce416a98a7b8d2e9b0
kmalhotra13 <k9@sukarm.com>
2019-04-19 08:41:16 -0500

    Creation of Storage folder

--- apps/round-up-saver.py
+++ apps/round-up-saver.py
@@ -3,11 +3,14 @@
 
 # Import necessary packages
 
-import os
+
 from datetime import datetime
-import json
-import requests
 from dotenv import load_dotenv
+import json
+import os
+import pprint
+import requests
+from twilio.rest import Client
 
 # Function definition
 
@@ -23,8 +26,8 @@ def round_up(my_amount): # <-- round up the cent value to the nearest dollar
 
 load_dotenv()
 
-TOKEN = os.environ.get("TOKEN")
-secret = os.environ.get("SECRET")
+TOKEN = os.environ.get("token")
+secret = os.environ.get("secret")
 
 checking_id = os.environ.get("FROM_ACCT_ID")
 checking_bal_id = f'abl_{checking_id}'
@@ -48,24 +51,31 @@ headers = {
 
 # Gather last transaction ID
 
-with open('last_tranID.txt') as file:
+with open('storage/last_tranID.txt') as file:
 	last_transaction = file.read()
 
-TRANSACTION_URL = f'{base_url}accounts/{checking_id}/transactions?before={last_transaction}'
+TRANSACTION_URL = f'{base_url}/accounts/{checking_id}/transactions?before={last_transaction}'
+
+# tran_response = requests.get(TRANSACTION_URL, headers=headers)
+
+# print(tran_response)
+
 
 total_savings = 0
+total_spend = 0
 tranIDs = []
 
 # Open sample json transaction data
 
 with open('test.txt') as json_file:
 	data=json.load(json_file)
-	
+
 # run transactions off json file
 
 	for p in data['transactions']:
 		if p['source'] == "card":
 			tranIDs.append(p["id"])
+			total_spend = total_spend + p['amount']
 			cents = tare_cents(p['amount'])			
 			if (cents) != 0:
 				rounded_val = round_up(cents)
@@ -78,14 +88,20 @@ with open('test.txt') as json_file:
 
 last_ID = tranIDs[0]
 
+total_spend_usd = "{0:.2f}".format(-(total_spend/100))
+total_spend_formatted = str(f'${total_spend_usd}')
+
+total_savings_usd = "{0:.2f}".format(total_savings/100)
+total_savings_formatted = str(f'${total_savings_usd}')
+
 # Save last transaction ID to text file
 
-with open('last_tranID.txt', 'w') as f:
+with open('storage/last_tranID.txt', 'w') as f:
 	f.write(last_ID)
 
 # Initialilze the transfer post request
 
-transfer_url = f"{base_url}transfers/"
+TRANSFER_URL = f"/{base_url}transfers/"
 
 # # Create payload to post total savings
 
@@ -95,10 +111,43 @@ transfer_url = f"{base_url}transfers/"
 # 	"amount": total_savings
 # }
 
-# post_response = requests.post(transfer_url, headers=headers, json=payload)
+# post_response = requests.post(TRANSFER_URL, headers=headers, json=payload)
 
 # print(post_response)
 # print(post_response.text)
 
 
+# Send text update
+
+# Initialize environment variables:
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID", "OOPS, please specify env var called 'TWILIO_ACCOUNT_SID'")
+TWILIO_AUTH_TOKEN  = os.environ.get("TWILIO_AUTH_TOKEN", "OOPS, please specify env var called 'TWILIO_AUTH_TOKEN'")
+SENDER_SMS  = os.environ.get("SENDER_SMS", "OOPS, please specify env var called 'SENDER_SMS'")
+RECIPIENT_SMS  = os.environ.get("RECIPIENT_SMS", "OOPS, please specify env var called 'RECIPIENT_SMS'")
+
+# AUTHENTICATE
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+# COMPILE REQUEST PARAMETERS (PREPARE THE MESSAGE)
+
+content = f"This is a message from GUASFCU. You spent {total_spend_formatted} and saved {total_savings_formatted}!"
+
+# ISSUE REQUEST (SEND SMS)
+
+message = client.messages.create(to=RECIPIENT_SMS, from_=SENDER_SMS, body=content)
+
+# PARSE RESPONSE
+
+pp = pprint.PrettyPrinter(indent=4)
+
+print("----------------------")
+print("SMS")
+print("----------------------")
+print("RESPONSE: ", type(message))
+print("FROM:", message.from_)
+print("TO:", message.to)
+print("BODY:", message.body)
+print("PROPERTIES:")
+pp.pprint(dict(message._properties))
 
