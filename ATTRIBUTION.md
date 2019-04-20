093836e2704fb2d9113829e53189f6ef2ea34987
kmalhotra13 <k9@sukarm.com>
2019-04-19 21:44:18 -0500

    Automated testing

--- ATTRIBUTION.md
+++ ATTRIBUTION.md
@@ -1,7 +1,11 @@
 # Attributions:
 
-### Documentation for API:
+### Documentation for Narmi API:
 
 Documentation for the GUASFCU API can be found at https://www.narmi.com/developers/
+
+The team at Narmi was extremely helpful in guiding me through this process, specifically with the SHA-256 encryption of the requests. 
+
+
 
 
---
+++ test/round-up-saver-test.py
@@ -0,0 +1,12 @@
+### The purpose of this testing application is to continuously test the functionality of the round up saver. 
+
+from apps.round-up-saver import tare_cents
+from apps.round-up-saver import round_up
+
+def test_tare_cents:
+	result = tare_cents(-2332)
+	assert result == 32
+
+def test_round_up:
+	result = round_up(32)
+	assert result == 68
